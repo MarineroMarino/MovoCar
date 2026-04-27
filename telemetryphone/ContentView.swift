aafd0cf53335e9beafd89398bc230ccfbd4e3c79
Marino Vargas Zapata <marinillo4@gmail.com>
2026-04-27 11:12:50 +0200

    Initial Commit

---
+++ telemetryphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  telemetryphone
+//
+//  Created by Marino Vargas Zapata on 27/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ telemetryphone/telemetryphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  telemetryphoneApp.swift
+//  telemetryphone
+//
+//  Created by Marino Vargas Zapata on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct telemetryphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
